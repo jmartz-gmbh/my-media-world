e292111399af736a8bf224c0faaf83e0d9f7c945
Jonathan Martz <git@jmartz.de>
2022-06-09 18:41:09 +0200

    update content verify form

--- app/src/pages/user/email/verify.vue
+++ app/src/pages/user/email/verify.vue
@@ -2,12 +2,19 @@
   <div class="vp-email-verify-token">
     <div class="grid grid-cols-6">
       <div class="col-span-6 text-center">
-        <input
-          type="text"
-          v-model="$route.params.token"
-          class="mx-auto px-2 py-2 bg-white"
-        />
-        <button @click="verify()">verify</button>
+        <div class="form-group w-full block">
+          <label>Token</label>
+          <input
+            type="text"
+            v-model="$route.params.token"
+            class="mx-auto px-2 py-2 bg-white"
+          />
+        </div>
+        <div class="buttons mt-2">
+          <button @click="verify()" class="bg-white text-black px-2 py-2">
+            verify
+          </button>
+        </div>
       </div>
     </div>
   </div>
