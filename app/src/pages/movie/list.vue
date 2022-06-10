b4d818902db59e4e1f75aa8437d8e7ded94a3f37
Jonathan Martz <git@jmartz.de>
2022-06-10 17:26:38 +0200

    update list headlines

--- app/src/pages/movie/list.vue
+++ app/src/pages/movie/list.vue
@@ -14,6 +14,9 @@
         <div class="tw-table block border border-black">
           <div class="row grid grid-cols-12 bg-gray-300 font-bold px-2 py-2">
             <div class="col col-span-12 md:col-span-6">Name</div>
+            <div class="col col-span-12 md:col-span-6">TMDB Rating</div>
+            <div class="col col-span-12 md:col-span-6">User Rating</div>
+            <div class="col col-span-12 md:col-span-6">Kommentar</div>
             <div class="col col-span-12 md:col-span-6">Action</div>
           </div>
           <div
@@ -21,9 +24,11 @@
             class="row grid grid-cols-12 font-bold px-2 py-2"
           >
             <div class="col col-span-12 md:col-span-4">{{ movie.name }}</div>
-            <div class="col col-span-12 md:col-span-2">{{ movie.tmdb_rating }}</div>
-            <div class="col col-span-12 md:col-span-2">{{ movie.comment }}</div>
+            <div class="col col-span-12 md:col-span-2">
+              {{ movie.tmdb_rating }}
+            </div>
             <div class="col col-span-12 md:col-span-2">{{ movie.rating }}</div>
+            <div class="col col-span-12 md:col-span-2">{{ movie.comment }}</div>
             <div class="col col-span-12 md:col-span-2 space-x-2">
               <button
                 @click="remove(movie.id)"
